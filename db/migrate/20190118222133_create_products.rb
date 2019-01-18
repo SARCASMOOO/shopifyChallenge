887cd3dbf9f33a337bea7dbd803236d802c4823b
SARCASMOOO <ssstevenstapleton@gmail.com>
2019-01-18 17:39:02 -0500

    Product added to DB

--- db/migrate/20190118222133_create_products.rb
+++ db/migrate/20190118222133_create_products.rb
@@ -2,7 +2,7 @@ class CreateProducts < ActiveRecord::Migration[5.1]
   def change
     create_table :products do |t|
       t.string :name
-      t.double :price
+      t.float :price, :limit => 30
       t.integer :inventory
       t.timestamps
     end

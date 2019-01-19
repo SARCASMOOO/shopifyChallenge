d8eb9c6810e7f5d2c7adcc8369fbd7d619952f18
SARCASMOOO <ssstevenstapleton@gmail.com>
2019-01-18 23:25:31 -0500

    Added todo's to product controller

--- app/controllers/product_controller.rb
+++ app/controllers/product_controller.rb
@@ -1,3 +1,13 @@
 class ProductController < ApplicationController
+  def products
+    #return all products or ones with inventory based on choice
+  end
 
+  def product
+    #return a specfic product
+  end
+
+  def purchase
+    #purchase a specific product 
+  end
 end

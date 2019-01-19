1b04e0741ee82283ff34736d2a28e0b479d18b45
SARCASMOOO <ssstevenstapleton@gmail.com>
2019-01-19 17:19:19 -0500

    purchase integrations test wrote

--- test/integration/purchase_flow_test.rb
+++ test/integration/purchase_flow_test.rb
@@ -1,6 +1,13 @@
 require 'test_helper'
 
 class PurchaseFlowTest < ActionDispatch::IntegrationTest
+  def setup
+     @banana = products(:banana)
+     @apple = products(:apple)
+     @cartA = carts(:cartA)
+     @cartB = carts(:cartB)
+  end
+
   test "validate checkout total" do
     get "/order", params: {productName: @banana.name,
       cartName: @cartA.name, amount: 2}
@@ -10,10 +17,24 @@ class PurchaseFlowTest < ActionDispatch::IntegrationTest
   end
 
   test "check invalid purchases" do
-    assert false
+    get "/order", params: {productName: @apple.name,
+      cartName: @cartA.name, amount: 2}
+
+    assert_no_difference 'CartsProduct.count' do
+      get '/buy',  params: {cartName: @cartA.name}
+    end
   end
 
   test "check product inventory does not change before checking out" do
-    assert false
+    #Check inventory does not change before buying
+    get "/order", params: {productName: @banana.name,
+      cartName: @cartA.name, amount: 2}
+    get "/product", params: {name: @banana.name}
+    assert_equal @bannana.inventory, json_response['product']['inventory']._to_i
+
+    #Check that buying will change inventory
+    get '/buy',  params: {cartName: @cartA.name}
+    get "/product", params: {name: @banana.name}
+    assert_equal @bannana.inventory - 2, json_response['product']['inventory']._to_i
   end
 end

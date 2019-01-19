5475a5ceb04137c99cf9b0fc649cb8a38e25580a
SARCASMOOO <ssstevenstapleton@gmail.com>
2019-01-18 23:10:20 -0500

    Product controller tests created

--- test/controllers/product_controller_test.rb
+++ test/controllers/product_controller_test.rb
@@ -5,4 +5,36 @@ class ProductControllerTest < ActionDispatch::IntegrationTest
      @bannana = products(:bannana)
      @apple = products(:apple)
   end
+
+  test "purchase too many apples" do
+    assert false
+  end
+
+  test "purchase when product with no inventory" do
+    assert false
+  end
+
+  test "purchase too much of a product" do
+    assert false
+  end
+
+  test "purchase a negative ammount of a product" do
+    assert false
+  end
+
+  test "purchase a product which does not exist" do
+    assert false
+  end
+
+  test "select a specfic product which exists" do
+    assert false
+  end
+
+  test "select a specfic product which does not exist" do
+    assert false
+  end
+
+  test "select all products which have inventory" do
+    assert false
+  end
 end

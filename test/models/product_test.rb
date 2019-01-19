8e08e9d554567a630f6ec6dd94f5d2fe8362bf77
SARCASMOOO <ssstevenstapleton@gmail.com>
2019-01-18 20:26:22 -0500

    Product model data validation tests

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,23 +1,39 @@
 require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
-  test "purchase inventory with enough in stock" do
-    assert false
+  def setup
+    @product = Products.new(name: "Apple", price: 1.96, inventory: 3)
   end
 
-  test "purchase inventory without enough in stock" do
-    assert false
+  test "valid product" do
+    assert @product.valid?
   end
 
-  test "handle if product does not exist" do
-    assert false
+  test "products should be unique" do
+    duplicate_product = @product.dup
+    duplicate_product.name = @product.name.upcase
+    @product.save
+    assert_not duplicate_product.valid?
   end
 
-  test "check product queries correct product" do
-    assert false
+  test "check if we can add a product with a negative price or zero" do
+    @product.price = -20
+    assert_not @product.valid?
+    @product.price = 0
+    assert_not @product.valid?
   end
 
-  test "query to get all products" do
-    assert false
+  test "check if we can add a product without a name, price, or quantity" do
+    @product.name = "     "
+    assert_not @product.valid?
+    @product.price = nil
+    assert_not @product.valid?
+    @product.inventory = nil
+    assert_not @product.valid?
+  end
+
+  test "check if name is too long" do
+    @product.name = "This name for a product is way too long for meeeeee"
+    assert_not @product.valid?
   end
 end

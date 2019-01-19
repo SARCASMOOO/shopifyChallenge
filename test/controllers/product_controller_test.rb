e13ea253438239b0b7b1be75ce91b35d70052c00
SARCASMOOO <ssstevenstapleton@gmail.com>
2019-01-18 22:23:01 -0500

    Created product controller

---
+++ test/controllers/product_controller_test.rb
@@ -0,0 +1,10 @@
+require 'test_helper'
+
+class ProductControllerTest < ActionDispatch::IntegrationTest
+  def setup
+     @bannana = products(:bannana)
+     @apple = products(:apple)
+  end
+
+  #
+end

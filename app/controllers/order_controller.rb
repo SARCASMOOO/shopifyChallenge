738d6f16a41f481d6be12abb69f33844b413511d
SARCASMOOO <ssstevenstapleton@gmail.com>
2019-01-19 15:00:33 -0500

    Orders implemented and tests passed

--- app/controllers/order_controller.rb
+++ app/controllers/order_controller.rb
@@ -1,5 +1,24 @@
 class OrderController < ApplicationController
   def addToOrder
-    # TODO: Take in product and cart ID to add a purchase to orders
+    productName = params[:productName]
+    cartName = params[:cartName]
+    amount = params[:amount]
+
+    tempCart = Cart.find_by(name: cartName)
+    tempProduct = Product.find_by(name: productName)
+
+    if tempProduct.nil? == false &&  tempCart.nil? == false
+      if tempProduct.inventory > 0 && amount.nil? == false
+        if amount.to_i > 0
+          @order = CartsProduct.new(amount: amount, product_id: tempProduct.id,
+          cart_id: tempCart.id)
+
+          @order.save
+          render json: {result: true} and return;
+        end
+      end
+    end
+
+    render json: {result: false};
   end
 end
--- test/controllers/order_controller_test.rb
+++ test/controllers/order_controller_test.rb
@@ -2,27 +2,30 @@ require 'test_helper'
 
 class OrderControllerTest < ActionDispatch::IntegrationTest
   def setup
-     @bannana = products(:bannana)
+     @banana = products(:banana)
      @apple = products(:apple)
      @cartA = carts(:cartA)
      @cartB = carts(:cartB)
   end
 
   test "Add a valid item to order" do
-    assert_difference 'Order.count', 1 do
-      get "/order", params: {product: @bannana, cart: cartA}
+    assert_difference 'CartsProduct.count', 1 do
+      get "/order", params: {productName: @banana.name,
+        cartName: @cartA.name, amount: 1}
     end
   end
 
   test "Add a invalid item to order" do
-    assert_no_difference 'Order.count' do
-      get "/order", params: {product: @bannana, cart: nil}
+    assert_no_difference 'CartsProduct.count' do
+      get "/order", params: {productName: @banana.name,
+        cartName: @cartA.name, amount: nil}
     end
   end
 
   test "Add a order which is out of stock" do
-    assert_no_difference 'Order.count' do
-      get "/order", params: {product: @apple, cart: cartB}
+    assert_no_difference 'CartsProduct.count' do
+      get "/order", params: {productName: @apple.name,
+        cartName: @cartB.name, amount: 1}
     end
   end
 end
